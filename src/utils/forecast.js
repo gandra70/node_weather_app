d54333ccae51ca174901b75f4458f170e99eb8b3
Dragan Drenjanin <drenjanindragan@gmail.com>
2021-12-30 11:49:29 +0100

    hide footer on mobile

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=c4054b998d8b5e479c6ffab215e38812&query='  + latitude + ',' + longitude
 
-    request({ url, json: true }, (error, { body} ) => {
+    request({ url, json: true }, (error, { body}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] +' | It is currently: ' + body.current.temperature + ' ℃ ( feels like ' + body.current.feelslike + '  ℃ ) | The humidity: ' + body.current.humidity + '%' 
+            callback(undefined, body.current.weather_descriptions[0] +' | It is currently: ' + body.current.temperature + ' ℃(feels like ' + body.current.feelslike + '  ℃) | The humidity: ' + body.current.humidity + '%' 
             + ' | Air pressure: ' + body.current.pressure + ' millibars' + ' | Wind speed:  '+  body.current.wind_speed + ' km/hour |'+ ' Direction: " '+ body.current.wind_dir + ' "')
         }
     })
@@ -30,7 +30,5 @@ const forecast2= (latitude, longitude, callback) => {
         }
     })
 }
-
-
 
 module.exports = forecast || forecast2
