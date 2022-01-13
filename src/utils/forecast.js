eda4f885b2ce511407b141f2fc52fb250159cc0b
Dragan Drenjanin <drenjanindragan@gmail.com>
2022-01-13 18:25:39 +0100

    forecast API

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,34 +1,53 @@
 const request = require('request')
 
-const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=c4054b998d8b5e479c6ffab215e38812&query='  + latitude + ',' + longitude
+// const forecast = (latitude, longitude, callback) => {
 
-    request({ url, json: true }, (error, { body}) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.current.weather_descriptions[0] +' | It is currently: ' + body.current.temperature + ' ℃(feels like ' + body.current.feelslike + '  ℃) | The humidity: ' + body.current.humidity + '%' 
-            + ' | Air pressure: ' + body.current.pressure + ' millibars' + ' | Wind speed:  '+  body.current.wind_speed + ' km/hour |'+ ' Direction: " '+ body.current.wind_dir + ' "')
-        }
+//     const url = 'http://api.weatherstack.com/current?access_key=e562704299f5e9a6bb484ff07e1d8694&query='  + latitude + ',' + longitude
+
+//     request({
+//         url,
+//         json: true
+//     }, (error, {body} = {}) => {
+//         if (error) {
+//             callback('Unable to connect to weather service!', undefined)
+//         } else if (body.error) {
+//             callback('Unable to find location', undefined)
+//         } else {
+//             callback(undefined, body.current.weather_descriptions[0] + ' | It is currently: ' + body.current.temperature + ' ℃(feels like ' + body.current.feelslike + '  ℃) | The humidity: ' + body.current.humidity + '%' +
+//                 ' | Air pressure: ' + body.current.pressure + ' millibars' + ' | Wind speed:  ' + body.current.wind_speed + ' km/hour |' + ' Direction: " ' + body.current.wind_dir + ' "')
+//         }
+//     })
+// }
+const getData = (url, callback) => {
+    //console.log(`Pokusaj na url: ${url}`)
+    request({ url, json: true }, (error, { body }) => {
+      // ovo prvo je error kada je istekao token
+      if (body.success === false) {
+        callback("Api Token Invalid", null);
+      } else if (error) {
+        callback('Unable to connect to weather service!', null);
+      } else if (body.error) {
+        callback('Unable to find location', null)
+      } else {
+        callback(null, body.current.weather_descriptions[0] + ' | It is currently: ' + body.current.temperature + ' ℃(feels like ' + body.current.feelslike + '  ℃) | The humidity: ' + body.current.humidity + '%'
+          + ' | Air pressure: ' + body.current.pressure + ' millibars' + ' | Wind speed:  ' + body.current.wind_speed + ' km/hour |' + ' Direction: " ' + body.current.wind_dir + ' "')
+      }
     })
-}
-
-
-const forecast2= (latitude, longitude, callback) => {
-    const url2 = 'http://api.weatherstack.com/current?access_key=e562704299f5e9a6bb484ff07e1d8694&query='  + latitude + ',' + longitude
-
-    request({ url2, json: true }, (error, { body} ) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.current.weather_descriptions[0] +' | It is currently: ' + body.current.temperature + ' ℃ ( feels like ' + body.current.feelslike + '  ℃ ) | The humidity: ' + body.current.humidity + '%' 
-            + ' | Air pressure: ' + body.current.pressure + ' millibars' + ' | Wind speed:  '+  body.current.wind_speed + ' km/hour |'+ ' Direction: " '+ body.current.wind_dir + ' "')
-        }
+  }
+  
+  const forecast = (latitude, longitude, callback) => {
+  
+    const apiKeys = ["c4054b998d8b5e479c6ffab215e38812", "e562704299f5e9a6bb484ff07e1d8694" ];
+  
+  
+    getData(`http://api.weatherstack.com/current?access_key=${apiKeys[0]}&query=${latitude},${longitude}`, (err, data) => {
+      if (err === "Api Token Invalid") {
+        return getData(`http://api.weatherstack.com/current?access_key=${apiKeys[1]}&query=${latitude},${longitude}`, callback)
+      } else {
+        return callback(err, data);
+      }
     })
-}
+  
+  }
 
-module.exports = forecast || forecast2
+module.exports = forecast
